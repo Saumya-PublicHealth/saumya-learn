b6dbb92366a03d5c76554fcaa876e84866cab271
Saumya-PublicHealth <69596095+Saumya-PublicHealth@users.noreply.github.com>
2020-08-12 22:15:05 +0530

    Add files via upload

---
+++ self_intro.py
@@ -0,0 +1,8 @@
+print ("hello world")
+print ("this is Saumya Ranjan Pani")
+print ("I am a public health professional, having nealy 6 yeasr professional experience!")
+print ("I am interested in: Human Resource for Health, Social Epidemiology, Data Analytics, Public Healht Nutrition, Costing Evaluation of Public Health Interventions, ML & AI and thier applications in the field of Public Health")   
+print ("thanks reading about me")
+print ("thanks for executing this code")
+input("press 'enter' to exit")
+
